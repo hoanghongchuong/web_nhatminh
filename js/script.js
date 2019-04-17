8e0de24fe9076a3c69c16e8bc0fff71f1b26991a
hoanghongchuong <chuonghoanhong@gmail.com>
2019-04-17 10:13:14 +0700

    a

--- js/script.js
+++ js/script.js
@@ -44,7 +44,43 @@ $(document).ready(function($){
 		            items:1,  
 		        }
 		    }
-		});  	
+		});
+	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  asNavFor: '.slider-nav',
+		 autoplay: true
+	});
+	$('.slider-nav').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: false,
+	  centerMode: true,
+	  focusOnSelect: true,
+	  responsive: [{
+ 			breakpoint: 1024,
+ 			settings: {
+ 				slidesToShow: 5,
+ 				slidesToScroll: 5,
+ 			}
+ 		}, {
+ 			breakpoint: 640,
+ 			settings: {
+ 				slidesToShow: 4,
+ 				slidesToScroll: 4,
+			}
+ 		}, {
+ 			breakpoint: 420,
+ 			settings: {
+ 				slidesToShow: 3,
+ 				slidesToScroll: 3,
+		}
+ 		}]
+
+	}); 	
 });
 
 var MobileMenu = function () {
